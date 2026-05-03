813973d89acd30c018721fc60ea5c91b557266d6
KXORATH <minecraftKXORATH@gmail.com>
2026-05-03 23:33:50 +0200

    Revert to 18:09 state and apply only vite-plugin-legacy with renderModernChunks: false

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'not dead', 'ie 11', 'chrome 49'],
+      renderModernChunks: false,
+    }),
+  ],
   base: '/dart4fun_competitions/',
   build: {
     target: 'es2015',
