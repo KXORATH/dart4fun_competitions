8f4fd079df69002bba31ca6f12fb897fee6f32fc
KXORATH <minecraftKXORATH@gmail.com>
2026-05-13 22:33:25 +0200

    fix: custom domain dart4fun.pl - remove legacy plugin, fix base path, add CNAME

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy({
-      targets: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'not dead', 'ie 11', 'chrome 38', 'safari 9'],
-      renderModernChunks: false,
-    }),
-    {
-      name: 'remove-crossorigin',
-      transformIndexHtml(html) {
-        return html.replace(/crossorigin="[^"]*"/g, '').replace(/crossorigin/g, '');
-      }
-    }
   ],
-  base: '/dart4fun_competitions/',
+  base: '/',
   build: {
     target: 'es2015',
   }
